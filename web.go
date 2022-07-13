892cf95941161f3d64d9bc5f549a7dc33fe13ec5
Chung <chung19941024@gmail.com>
2022-07-14 01:26:31 +0800

    login form

--- web.go
+++ web.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -20,8 +21,33 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello astaxie!") // return this string as response
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println("----", t.Execute(w, nil))
+	} else if r.Method == "POST" {
+		r.ParseForm() // if no this line, can't read form value
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+
+		v := r.Form
+		v.Set("name", "Ava")
+		v.Add("friend", "Jess")
+		v.Add("friend", "Sarah")
+		v.Add("friend", "Zoe")
+		// v.Encode() == "name=Ava&friend=Jess&friend=Sarah&friend=Zoe"
+		fmt.Println("v.Get('name')", v.Get("name"))
+		fmt.Println("v.Get('friend')", v.Get("friend"))
+		fmt.Println("v['friend']", v["friend"])
+	} else {
+		fmt.Fprintf(w, "unknown method")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
